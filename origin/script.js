07d86be2a5c466a81162712bb25c3d55905180cd
panzhuo <zhuo_pan@icloud.com>
2025-03-26 17:31:06 +0800

    update some stuff

--- origin/script.js
+++ origin/script.js
@@ -1,22 +1,14 @@
 const checkbox = document.getElementsByClassName("checkbox")
 
-alert(document.getElementById("c1").parentElement) //获取到label对象
-document.getElementById("c1").parentElement.classList.add("completed")
-
-
 for(let i=0;i<checkbox.length;i++){
     checkbox[i].addEventListener('change',function(){
         const taskText = this.nextSibling.textContent.trim();
-        alert("sibliing:",this.nextSibling)
         if(this.checked){
-            // alert(`"${taskText}" 已完成`);
-            // 增加样式
-            this.nextSibling.style.textDecoration = "line-through";
-            this.nextSibling.style.color = "#888";
+            console.log(`"${taskText}" 已完成`);
+            this.parentElement.classList.add('completed')
         }else{
-            alert(`"${taskText}" 未完成`);
-            this.nextSibling.style.textDecoration = "none";
-            this.nextSibling.style.color = "#000";
+            console.log(`"${taskText}" 未完成`);
+            this.parentElement.classList.remove('completed');
         }
     })
     // checkbox[i].checked=true;
