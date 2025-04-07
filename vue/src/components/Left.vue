1698d17580ca48772438e6da1cfcc3037d45e69b
panzhuo <zhuo_pan@icloud.com>
2025-04-07 18:04:08 +0800

    基本模型

---
+++ vue/src/components/Left.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Left -- </h1>
+</template>
+<script lang="ts">
+    export default {
+        name:'Left',
+    }
+</script>
+<style>
+</style>
---
+++ vue/src/components/Right.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>Right -- </h1>
+</template>
+<script></script>
+<style></style>
